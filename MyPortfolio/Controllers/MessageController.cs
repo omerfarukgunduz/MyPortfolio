dfaaae7861f2d6a0cdcc1acfc040fd9f0b537ee6
Ömer Faruk Gündüz <omerfarukgunduz034@gmail.com>
2024-07-24 14:46:21 +0300

    Maili cevaplamak istediğimizde alıcı mail adresini yazmama gerek yok dolu olarak geliyor.

--- MyPortfolio/Controllers/MessageController.cs
+++ MyPortfolio/Controllers/MessageController.cs
@@ -50,43 +50,47 @@ namespace MyPortfolio.Controllers
 			return View(value);	
 		}
 
-		[HttpGet]
-		public IActionResult MessageSend(int id) 
-		{
-		return View(context.Messages.Find(id));
-		}
+        [HttpGet]
+        public IActionResult MessageSend(int id, string email)
+        {
+            var message = context.Messages.Find(id);
+            var model = new Mail
+            {
+                ReceiverMail = email
+            };
 
-		[HttpPost]
-		public IActionResult MessageSend(Mail mail)
-		{
-			var mailadrs = context.Tokens.FirstOrDefault()?.MailAddress;
-			var ownername = context.Tokens.FirstOrDefault()?.OwnerName;
+            return View(model);
+        }
 
-			MimeMessage mimeMessage = new MimeMessage();
-			MailboxAddress mailboxAddressFrom = new MailboxAddress($"{ownername}", $"{mailadrs}");
-			mimeMessage.From.Add(mailboxAddressFrom);
+        [HttpPost]
+        public IActionResult MessageSend(Mail mail)
+        {
+            var mailadrs = context.Tokens.FirstOrDefault()?.MailAddress;
+            var ownername = context.Tokens.FirstOrDefault()?.OwnerName;
 
-			MailboxAddress mailboxAddressTo = new MailboxAddress("Visitor", mail.ReceiverMail);
-			mimeMessage.To.Add(mailboxAddressTo);
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress($"{ownername}", $"{mailadrs}");
+            mimeMessage.From.Add(mailboxAddressFrom);
 
-			var bodyBuilder = new BodyBuilder();
-			bodyBuilder.TextBody=mail.Body;
-			mimeMessage.Body=bodyBuilder.ToMessageBody();
+            MailboxAddress mailboxAddressTo = new MailboxAddress("Visitor", mail.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
 
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mail.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
 
+            mimeMessage.Subject = mail.Subject;
 
-			mimeMessage.Subject = mail.Subject;
+            var password = context.Tokens.FirstOrDefault()?.Password;
 
-			var password = context.Tokens.FirstOrDefault()?.Password;
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate($"{mailadrs}", $"{password}");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
 
-			SmtpClient client = new SmtpClient();
-			client.Connect("smtp.gmail.com", 587, false);
-			client.Authenticate($"{mailadrs}", $"{password}");
-			client.Send(mimeMessage);
-			client.Disconnect(true);
+            return RedirectToAction("Inbox");
+        }
 
-			return RedirectToAction("Inbox");
-		}
-
-	}
+    }
 }
